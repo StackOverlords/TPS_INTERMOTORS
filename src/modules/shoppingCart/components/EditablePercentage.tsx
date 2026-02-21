04754010c863b0558102819b65c1dbda6c68c216
olivio-git <subelzaolivitocabezas@gmail.com>
2026-02-21 18:42:02 -0400

    fix: hardcoded 100% inc

--- src/modules/shoppingCart/components/EditablePercentage.tsx
+++ src/modules/shoppingCart/components/EditablePercentage.tsx
@@ -5,7 +5,7 @@ export const EditablePercentage: React.FC<Omit<EditableFieldProps, 'type' | 'for
         {...props}
         type="number"
         formatter={(value) => typeof value === 'number' ? value.toFixed(2) : parseFloat(value.toString()).toFixed(2)}
-        numberProps={{ min: 0, max: 100, step: 0.1, ...props.numberProps }}
+        numberProps={{ min: 0, step: 0.1, ...props.numberProps }}
         focusNextOnEnter={true}
     />
 );
