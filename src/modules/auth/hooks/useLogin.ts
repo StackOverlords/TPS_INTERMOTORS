f12acd30047d9883aafb601af9d0fac32b6f901f
olivio-git <subelzaolivitocabezas@gmail.com>
2026-05-22 18:25:10 -0400

    fix(auth): useLogin hook

--- src/modules/auth/hooks/useLogin.ts
+++ src/modules/auth/hooks/useLogin.ts
@@ -1,20 +1,13 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "@/lib/reactQueryConfig";
 import type { Login } from "../types/login.types";
 import { authService } from "../services/authService";
 
-export const AUTH_QUERY_KEYS = {
-    auth: ['auth'] as const,
-} as const;
-
 export const useLogin = () => {
-    const queryClient = useQueryClient();
-
     return useMutation({
         mutationFn: (credentials: Login) => authService.login(credentials),
         onSuccess: () => {
-
-            queryClient.invalidateQueries({ queryKey: AUTH_QUERY_KEYS.auth });
-
+            queryClient.clear();
         },
     });
 };
